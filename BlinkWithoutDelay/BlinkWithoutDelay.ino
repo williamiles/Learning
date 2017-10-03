b96559b5b05ba2d31f0cd29963a38f4bbccc4d36
William Miles <billewille@hotmail.com>
2017-10-03 20:09:14 +0200

    Deldet

--- BlinkWithoutDelay/BlinkWithoutDelay.ino
+++ BlinkWithoutDelay/BlinkWithoutDelay.ino
@@ -1,8 +1,23 @@
 /* Blink without Delay
 
+ Turns on and off a light emitting diode (LED) connected to a digital
+ pin, without using the delay() function.  This means that other code
+ can run at the same time without being interrupted by the LED code.
 
-  http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
-*/
+ The circuit:
+ * Use the onboard LED.
+ * Note: Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino model, check
+  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
+
+
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
+ */
 
 // constants won't change. Used here to set a pin number :
 const int ledPin =  LED_BUILTIN;// the number of the LED pin
@@ -23,7 +38,11 @@ void setup() {
 }
 
 void loop() {
- which you want to
+  // here is where you'd put code that needs to be running all the time.
+
+  // check to see if it's time to blink the LED; that is, if the
+  // difference between the current time and last time you blinked
+  // the LED is bigger than the interval at which you want to
   // blink the LED.
   unsigned long currentMillis = millis();
 
